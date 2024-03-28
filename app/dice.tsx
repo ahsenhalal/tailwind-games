86119eafc44164dc53ed065e79a047eb0c6523d2
ahsenhalal <ahsennhalal@gmail.com>
2024-03-29 01:17:19 +0300

    tailwind-games

--- app/dice.tsx
+++ app/dice.tsx
@@ -1,12 +1,16 @@
-export default function Dice (){
+
+
+export default function Dice(props:any){
+
+  //console.log("props", props)
+  // #59E391
+
   return (
-<>
-
-<div className="bg[#FFFFFF] w-[35px] h-[35px] shadow-xl rounded-md flex justify-center">
-  <h2 className="font-bold text-2xl">6</h2>
-
-</div>
-</>
-
+      <>
+       <div className={`${props.isSelected === true ? "bg-[#59E391]" : "bg-[#FFFFFF]"} w-[35px] h-[35px] shadow-xl rounded-md flex justify-center`} 
+            onClick={(event) => props.toggle(event.target, props.id)}>
+          <h2 className="font-bold text-2xl">{props.number}</h2>
+       </div>
+      </>
   )
 }
